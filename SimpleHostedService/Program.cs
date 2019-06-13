bd52045e9bc5806a50cc9c51c9031a4470b50e12
teodoran <teodor@andeelstad.com>
2019-06-13 06:35:38 +0200

    A simple hosted service contrasting the console application

---
+++ SimpleHostedService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Examples.SimpleHostedService
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder();
+            var host = hostBuilder
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MyHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                });
+
+            await host.RunConsoleAsync();
+        }
+    }
+}
