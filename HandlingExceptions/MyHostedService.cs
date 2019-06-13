b3ae1a764df142945d6a0c9bace49a9ae7fde004
teodoran <teodor@andeelstad.com>
2019-06-13 06:36:09 +0200

    Show exception handling and basic configuration

---
+++ HandlingExceptions/MyHostedService.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Examples.HandlingExceptions
+{
+    public class MyHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger _logger;
+        private readonly Random _random;
+        private Timer _timer;
+
+        public MyHostedService(ILogger<MyHostedService> logger)
+        {
+            _logger = logger;
+            _random = new Random();
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("MyHostedService Started");
+
+            _timer = new Timer(
+                callback: DoWork,
+                state: null,
+                dueTime: TimeSpan.Zero,
+                period: TimeSpan.FromSeconds(2));
+
+            await Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("MyHostedService Stopped");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            await Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _logger.LogInformation("MyHostedService Disposed");
+
+            _timer?.Dispose();
+        }
+
+        private void DoWork(object state)
+        {
+            try
+            {
+                MightFail();
+                _logger.LogInformation("MyHostedService is working");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "MyHostedService failed");
+            }
+        }
+
+        private void MightFail()
+        {
+            if (_random.Next(1, 4) == 1)
+            {
+                throw new Exception("I'm a big bad exception!");
+            }
+        }
+    }
+}
---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Examples.HandlingExceptions
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder();
+            var host = hostBuilder
+                .ConfigureAppConfiguration((context, config) => config
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true)
+                    .AddJsonFile("appsettings.Development.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(args))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MyHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                });
+
+            await host.RunConsoleAsync();
+        }
+    }
+}
